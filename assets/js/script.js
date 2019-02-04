35cd41f27c9a6dccd0bbf91423382476bea1230b
geoffroyvilain <vilaingeoffroy02@gmail.com>
2019-02-04 15:51:25 +0100

    exo corrigé

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 $(function(){
-  //fonctionne au doubleclick
+  //fonctionne au doubleclick pour donner une taille de 500 px de largeur à l'image
   $('#image').dblclick(function(){
-    $('#image').width(500);
+    $(this).width(500);
+//#image ou mieux (this)     // ou .css('width','500px'); à la place de width
   });
 });
